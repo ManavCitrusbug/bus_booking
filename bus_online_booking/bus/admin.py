114367f47ac58a9a8f2cb85d0cc842d9f76b7f5c
unknown <manav.citrusbug@gmail.com>
2022-05-11 18:55:46 +0530

    validations

---
+++ bus_online_booking/bus/admin.py
@@ -0,0 +1,15 @@
+
+from django.contrib import admin
+from .models import *
+
+@admin.register(Transport)
+class Transports(admin.ModelAdmin):
+        list_display=['transport_name','number_plate','seats_available','price_per_person','bus_category','available','date_time_dpt']
+
+@admin.register(Category)
+class categorys(admin.ModelAdmin):
+        list_display=['category']
+
+@admin.register(Booked_bus)
+class Busbooking(admin.ModelAdmin):
+        list_display=['busname','name','address','phone','age','book_date_time','paid','user']
